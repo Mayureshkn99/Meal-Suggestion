fdb4ea7295446d18e077ff96fd79e121b0a0c683
Mayuresh <mayureshkn99@gmail.com>
2021-11-03 18:54:13 +0530

    Initial Landing Page Added kv file

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-import kivy
+"""Initial Landing Page"""
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+
+class BoxLayoutExample(BoxLayout):
+    pass
+
+
+class MealSuggestion(App):
+    pass
+
+
+MealSuggestion().run()
