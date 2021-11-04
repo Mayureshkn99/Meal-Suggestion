5c72511f309826e6c6b67cd77d278f592d107508
Mayuresh <mayureshkn99@gmail.com>
2021-11-05 00:28:46 +0530

    Changed kv file linking to Builder Added Screens Added Meal Type Selection Page Added Screen Transition functionality Added Back Functionality

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-"""Initial Landing Page"""
+"""An App that helps you decide what dish to have for your meal"""
 
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
 
 
-class BoxLayoutExample(BoxLayout):
+class HomePage(Screen):
     pass
 
 
-class MealSuggestion(App):
+class MealTypeSelectionPage(Screen):
     pass
 
 
-MealSuggestion().run()
+class WindowManager(ScreenManager):
+    def __init__(self, **kwargs):
+        super(WindowManager, self).__init__(**kwargs)
+        Window.bind(on_keyboard=self.on_key)
+
+    def on_key(self, window, key, *args):
+        if key == 27:  # the esc key
+            if self.current_screen.name == "HomePage":
+                return False  # exit the app from this page
+            elif self.current_screen.name == "MealTypeSelectionPage":
+                self.current = "HomePage"
+                return True  # do not exit the app
+
+
+kv = Builder.load_file("mealsuggestion.kv")
+
+
+class MealSuggestionApp(App):
+    def build(self):
+        return kv
+
+
+if __name__ == "__main__":
+    MealSuggestionApp().run()
