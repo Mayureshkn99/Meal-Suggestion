0a4adff9747797ce71b96862b50be090a176688d
Mayuresh <mayureshkn99@gmail.com>
2021-11-09 01:30:33 +0530

    Completed Add Meal for Restaurant and Home-made

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
 from kivy.uix.button import Button
 
+DATABASE_CHANGED = True
 
 class HomePage(Screen):
     pass
@@ -16,22 +17,49 @@ class MealTypeSelectionPage(Screen):
     pass
 
 
-class AddHomeMadeMealPage(Screen):
-    pass
-
-
 class AddRestaurantMealPage(Screen):
+
+    meal_types = []
 
     def __init__(self, **kwargs):
         super(AddRestaurantMealPage, self).__init__(**kwargs)
         self.store = JsonStore("database.json")
 
+    def get_meal_types(self, _, value, meal_type):
+        if value:
+            self.meal_types.append(meal_type)
+        else:
+            self.meal_types.remove(meal_type)
+
     def add_meal(self):
-        self.store.put(self.dish_name.text,
-                       restaurant_name=self.restaurant_name.text)
-        # dish_name = self.dish_name.text
-        # self.store.put("Dishes",
-        #                str(self.dish_name.text) = "")
+        self.store.put(self.ids.dish_name.text,
+                       meal_type=self.meal_types,
+                       restaurant_name=self.ids.restaurant_name.text,
+                       restaurant_number=self.ids.restaurant_number.text,
+                       delivery_links=self.ids.delivery_links.text)
+
+
+class AddHomeMadeMealPage(Screen):
+
+    meal_types = []
+
+    def __init__(self, **kwargs):
+        super(AddHomeMadeMealPage, self).__init__(**kwargs)
+        self.store = JsonStore("database.json")
+
+    def get_meal_types(self, _, value, meal_type):
+        if value:
+            self.meal_types.append(meal_type)
+        else:
+            self.meal_types.remove(meal_type)
+
+    def add_meal(self):
+        self.store.put(self.ids.dish_name.text,
+                       meal_type=self.meal_types,
+                       ingredients=self.ids.ingredients.text,
+                       receipe=self.ids.recipe.text)
+        global DATABASE_CHANGED
+        DATABASE_CHANGED = True
 
 
 class ViewMealsPage(Screen):
@@ -39,13 +67,24 @@ class ViewMealsPage(Screen):
         super(ViewMealsPage, self).__init__(**kwargs)
 
     def on_enter(self):
-        self.store = JsonStore("database.json")
-        meals = self.store.keys()
-        for meal in meals:
-            self.meal = Button(text=meal, size_hint=(0.9, None), height="50dp")
-            self.grid.add_widget(self.meal)
-            self.delete = Button(text = "x", size_hint=(0.1, None), height="50dp")
-            self.grid.add_widget(self.delete)
+        global DATABASE_CHANGED
+        if DATABASE_CHANGED:
+            DATABASE_CHANGED = False
+            self.store = JsonStore("database.json")
+            meals = self.store.keys()
+            for meal in meals:
+                self.meal = Button(text=meal, size_hint=(0.9, None), height="50dp", on_release=self.btn_press)
+                self.ids.grid.add_widget(self.meal)
+                self.delete = Button(text = "x", size_hint=(0.1, None), height="50dp")
+                self.ids.grid.add_widget(self.delete)
+
+    def btn_press(self, _):
+        self.manager.current = "EditRestaurantMealPage"
+        self.manager.transition.direction = "left"
+
+
+class EditRestaurantMealPage(Screen):
+    pass
 
 
 class SuggestionPage(Screen):
@@ -80,6 +119,10 @@ class WindowManager(ScreenManager):
                 return True  # do not exit the app
             elif self.current_screen.name == "ViewMealsPage":
                 self.current = "HomePage"
+                self.transition.direction = "right"
+                return True  # do not exit the app
+            elif self.current_screen.name == "EditRestaurantMealPage":
+                self.current = "ViewMealsPage"
                 self.transition.direction = "right"
                 return True  # do not exit the app
             elif self.current_screen.name == "SuggestionPage":
